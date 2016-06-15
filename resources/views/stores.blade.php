e281fe3e9645bba1ab820946504dccb534b18a03
Ravi Heer <ravi.heer@logicielsolutions.co.in>
2016-06-15 12:45:52 +0530

    css changes

--- resources/views/stores.blade.php
+++ resources/views/stores.blade.php
@@ -32,20 +32,17 @@
 		<div class="store_right">
 			<div class="subcription">
 				<h2>Couponlandmark Coupon Subscription</h2>
-				<div>
+				<p>
 					Email Adress<br>
 					<input type="text" name="">
-				</div>
-				<div>
+				</p>
+				<p>
 					First Name<br>
 					<input type="text" name="">
-				</div>
-				<div>
-					
-				</div>
-				<div>
+				</p>
+				<p>
 					<button type="submit">SUBMIT</button>
-				</div>
+				</p>
 			</div>
 		</div>
 	</div>
