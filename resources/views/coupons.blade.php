fca32de3f828822ad4094aea882de90e7747ed82
Ravi Heer <ravi.heer@logicielsolutions.co.in>
2016-06-15 14:57:18 +0530

    css changes in coupons

--- resources/views/coupons.blade.php
+++ resources/views/coupons.blade.php
@@ -3,6 +3,13 @@
 @section('content')
 <!-- Stores Start -->
 <div style="width:100%">
+	<!-- Image -->
+	<div class="store_top_image">
+		<a href="#">
+				<img src="http://frooji.com/wp-content/uploads/2016/05/18225.gif">
+		</a>
+	</div>
+	<!-- Heading -->
 	<h3 style="text-align:center;width:76%;text-align:center;float:left;padding-top:30px;">
 		<b>Coupons in the "Apparel" Category</b>
 	</h3>
@@ -10,58 +17,58 @@
 <div class="coupon_left">
 
 	<?php for($i = 1; $i <= 1 ; $i++) { ?>
-	<div class="coupon">
-		<p class="company_name">
-			<a href="">CuddlDuds</a>
-		</p>
-		<div class="store_holder">
-			<a href="">
-				<img src="http://frooji.com/wp-content/uploads/2016/05/18736.gif">
-			</a>
-			<div class="frooji_label">
-				25% off
-				<br>Code
-			</div>
-		</div>
-		<div class="coupon_title">
-			<h3>
+		<div class="coupon">
+			<p class="company_name col-sm-12">
+				<a href="">CuddlDuds</a>
+			</p>
+			<div class="store_holder col-sm-2">
 				<a href="">
-					25% off Men's Layers Plus Free Shipping
+					<img src="http://frooji.com/wp-content/uploads/2016/05/18736.gif">
 				</a>
-			</h3>
-		</div>
+				<div class="frooji_label">
+					25% off
+					<br>Code
+				</div>
+			</div>
+			<div class="coupon_title col-sm-10">
+				<h3>
+					<a href="">
+						25% off Men's Layers Plus Free Shipping 25% off Men's Layers Plus Free Shipping 25% off Men's Layers Plus Free Shipping 25% off Men's Layers Plus Free Shipping 25% off Men's Layers Plus Free Shipping 25% off Men's Layers Plus Free Shipping 25% off Men's Layers Plus Free Shipping 25% off Men's Layers Plus Free Shipping 25% off Men's Layers Plus Free Shipping 25% off Men's Layers Plus Free Shipping 
+					</a>
+				</h3>
+			</div>
 
-		<div class="use_coupon">
-			<a href="">
-				USE COUPON
-			</a>
+			<div class="use_coupon">
+				<a href="">
+					USE COUPON
+				</a>
+			</div>
+			<span class="deal_exp">Exp: 09/29/2016
+				<a href="" class="deal_star">
+					<img src="http://frooji.com/wp-content/plugins/wp-favorite-posts/img/fav-empty.png"/>
+				</a>
+			</span>	
+			<span class="coupon_views">4 total views, 1 today</span>
+			<div class="coupon_footer">
+				<div class="thumb_up">
+					<a href=""><div></div></a>
+				</div>
+				<div class="thumb_down">
+					<a href=""><div></div></a>
+				</div>
+				<div class="success">
+					<span>100%</span>
+					<br>success
+				</div>
+				<div class="comment">
+					comment
+				</div>
+			</div>
+			<div class="add_comment">
+				<a href="">Add Comment</a>
+			</div>
 		</div>
-		<span class="deal_exp">Exp: 09/29/2016
-			<a href="" class="deal_star">
-				<img src="http://frooji.com/wp-content/plugins/wp-favorite-posts/img/fav-empty.png"/>
-			</a>
-		</span>	
-		<span class="coupon_views">4 total views, 1 today</span>
-		<div class="coupon_footer">
-			<div class="thumb_up">
-				<a href=""><div></div></a>
-			</div>
-			<div class="thumb_down">
-				<a href=""><div></div></a>
-			</div>
-			<div class="success">
-				<span>100%</span>
-				<br>success
-			</div>
-			<div class="comment">
-				comment
-			</div>
-		</div>
-		<div class="add_comment">
-			<a href="">Add Comment</a>
-		</div>
-	</div>
-	<?php } ?>
+		<?php } ?>
 
 	<!-- <div id="comment_wrap">
 		<h1>Comment</h1>
